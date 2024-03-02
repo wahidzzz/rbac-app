5da9623a08b0d6ed3ff9a84ed019ac9144cced5f
wahid <wahid.shaikh@crimsoni.com>
2024-03-02 16:23:38 +0530

    feat: init commit

--- app/user/page.js
+++ app/user/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Base Page /Sign up Page
+      only user can see this
     </main>
   );
 }
